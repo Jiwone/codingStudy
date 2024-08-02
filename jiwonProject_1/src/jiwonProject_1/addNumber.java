2a5955b7c49a6545661dac05da75cea612c5d5c9
jiwon <rkdwldnjs922@naver.com>
2024-08-02 21:13:56 +0900

    수정

--- jiwonProject_1/src/jiwonProject_1/addNumber.java
+++ jiwonProject_1/src/jiwonProject_1/addNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class addNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		
