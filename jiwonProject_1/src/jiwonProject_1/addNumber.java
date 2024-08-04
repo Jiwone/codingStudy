bc793510a9a1fa3ea23d04e42d05fba4ca1315a9
jiwon <rkdwldnjs922@naver.com>
2024-08-04 14:07:36 +0900

    2024-08-04

--- jiwonProject_1/src/jiwonProject_1/addNumber.java
+++ jiwonProject_1/src/jiwonProject_1/addNumber.java
@@ -10,11 +10,14 @@ public class addNumber {
 		int N = sc.nextInt();
 		
 		String sNum = sc.next();
+		//toCharArray : 문자열을 배열로 만들어서 반환.
 		char[] cNum = sNum.toCharArray();
 		int sum = 0;
-		
+		//-'0' 을 하는 이유 : char형을 int형으로 바꾸려면 아스키코드로 변환됨.
+		// 1=> 49. '0' 이면 48임. 49-48 =1 .. 
 		for(int i=0 ; i<cNum.length; i++) {
 			sum +=cNum[i]-'0';
+			
 		}
 		System.out.print(sum);
 	}
